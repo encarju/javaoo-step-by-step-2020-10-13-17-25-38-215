b040c9afc619277c96dba61a91361974d282338c
encarju <justine.encarnacion@oocl.com>
2020-10-14 16:02:11 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE3 override introduce method in Worker

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,9 +1,14 @@
 package practice03;
 
-public class Worker extends Person{
+public class Worker extends Person {
 
     public Worker(String name, int age) {
         super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a Worker. I have a job.");
     }
 }
 
