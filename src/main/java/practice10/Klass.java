a811a2882efceb16896d72b4aeff4e5b19ccaebb
encarju <justine.encarnacion@oocl.com>
2020-10-14 20:56:10 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE10 Add assignLeader method in Klass class

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -2,6 +2,8 @@ package practice10;
 
 public class Klass {
     private int number;
+    private Student leader;
+    private Student member;
 
     public Klass(int number) {
         this.number = number;
@@ -11,9 +13,26 @@ public class Klass {
         return number;
     }
 
+
     public String getDisplayName() {
         return String.format("Class %d", number);
     }
 
+    public Student getLeader() {
+        return this.leader;
+    }
 
+    public void assignLeader(Student leader) {
+        if(leader.getKlass().number == this.number) {
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+
+    public void appendMember(Student member) {
+        this.member = member;
+    }
 }
