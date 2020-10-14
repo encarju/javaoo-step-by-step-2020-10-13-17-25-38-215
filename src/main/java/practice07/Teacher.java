25c49e3bdf646d1281e983e031e5c58f852f8653
encarju <justine.encarnacion@oocl.com>
2020-10-14 17:39:00 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE7 Add and update methods in Teacher Class

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,14 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
