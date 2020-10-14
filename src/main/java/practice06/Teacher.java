fdc18fc9687e98b90b73ef5f8ea8b64b84bde4d0
encarju <justine.encarnacion@oocl.com>
2020-10-14 17:03:52 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE6 Add introduce method in Teacher Class

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -12,4 +12,8 @@ public class Teacher extends Person {
         return klass;
     }
 
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), klass);
+    }
 }
