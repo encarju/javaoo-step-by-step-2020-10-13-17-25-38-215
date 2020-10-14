9e6403cd80d362a5747447fc121f52ca04188007
encarju <justine.encarnacion@oocl.com>
2020-10-14 17:29:59 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE7 Add and update method of Student Class

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,14 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
