1e4e0d185e0cd7cdf1c7e732ce20be62c86807b2
encarju <justine.encarnacion@oocl.com>
2020-10-14 20:47:27 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE9 Update and add introduce,introduceWith method in Teacher Class

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,31 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return klass==null ?
+                String.format("%s I am a Teacher. I teach No Class.", super.introduce()) :
+                String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), klass.getNumber());
+    }
+
+    public String introduceWith(Student student) {
+        return this.klass==student.getKlass() ?
+                String.format("%s I am a Teacher. I teach %s.",super.introduce(),student.getName()) :
+                String.format("%s I am a Teacher. I don't teach %s.",super.introduce(),student.getName());
+    }
 }
