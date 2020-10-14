464ce3e0ddc1026edbf156fa4d7ceb20eca8e93e
encarju <justine.encarnacion@oocl.com>
2020-10-14 19:55:47 +0800

    [JAVAOOP][ENCARJU] feature: PRACTICE9 Add and update methods of Person and Klass Class

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,23 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
 }
